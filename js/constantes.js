cc3efb3ff356ec341921dd77fe86646f22d397c0
angelexa302 <joseluisnavarta@hotmail.com.ar>
2025-01-06 10:47:22 -0300

    Agregando modulos, constantes js

---
+++ js/constantes.js
@@ -0,0 +1,6 @@
+export const PI = Math.PI;
+export let usuario = "Jose";
+export let password = "qwerty";
+export function saludar() {
+    console.log("Hola soy +EMA6");
+}
---
+++ js/modulos.js
@@ -0,0 +1,8 @@
+import { PI, password, usuario } from "./constantes.js";
+import {aritmetica as a} from "./aritmetica.js"
+console.log("archivo js");
+console.log(PI);
+console.log(usuario);
+console.log(password);
+console.log(a.suma(5,6));
+console.log(a.resta(10,5));
